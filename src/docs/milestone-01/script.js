2b102746dcfb807e729a5592ec49eaef60605bce
Nikita Singh <nikitas@vl965-172-31-246-115.wireless.umass.edu>
2024-04-02 16:49:07 -0400

    added content and formatting stuff

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -15,58 +15,69 @@ array.push(dataRequirements);
 array.push(wireFrames);
 array.push(realWorld);
 array.push(integrativeExperience);
-for (let i = 0; i < array.length; i++){
-    const theP = document.createElement("p");
-    //if (i % 2 === 0){
-      //  array[i].classList.add("boxesWhite");
-   // }
- //   else{
-  //      array[i].classList.add("boxesMaroon");
-  //  }
-    if (i === 0){
-        array[i].classList.add("#boxesWhite");
-        const node = document.createTextNode("faefa");
-        theP.appendChild(node);
-        array[i].appendChild(theP);
+
+for (let i = 0; i < array.length; i++) {
+    const section = array[i];
+    if (i % 2 === 0){
+        array[i].classList.add("borderbox");
     }
-    if (i === 1){
-        array[i].classList.add("#boxesMaroon");
-        const node = document.createTextNode(" eef");
-        theP.appendChild(node);
-        array[i].appendChild(theP);
-    }
-    if (i === 2){
-        array[i].classList.add("boxesWhite");
-        const node = document.createTextNode(" fdn");
-        theP.appendChild(node);
-        array[i].appendChild(theP);
-    }
-    if (i === 3){
-        array[i].classList.add("boxesMaroon");
-        const node = document.createTextNode("g ");
-        theP.appendChild(node);
-        array[i].appendChild(theP);
-    }
-    if (i === 4){
-        array[i].classList.add("boxesWhite");
-        const node = document.createTextNode("fdnsn ");
-        theP.appendChild(node);
-        array[i].appendChild(theP);
-    }
-    if (i === 5){
-        array[i].classList.add("boxesMaroon");
-        const node = document.createTextNode(" bszsb");
-        theP.appendChild(node);
-        array[i].appendChild(theP);
+    else{
+        array[i].classList.add("unborderbox");
     }
 }
-let counter = 0;
-function dynamicCounter(){
-    counter = counter + 1;
-}
-const counterElem = document.getElementById("counter");
-//counterElem.createTextNode(counter);
-let button = document.createElement("input");
-button.type = "button";
-button.addEventListener("click", dynamicCounter);
-counterElem.appendChild(button);
+
+// for (let i = 0; i < array.length; i++){
+//     const theP = document.createElement("p");
+//     //if (i % 2 === 0){
+//       //  array[i].classList.add("boxesWhite");
+//    // }
+//  //   else{
+//   //      array[i].classList.add("boxesMaroon");
+//   //  }
+//     if (i === 0){
+//         array[i].classList.add("borderbox");
+//         //const node = document.createTextNode("faefa");
+//         theP.appendChild(node);
+//         array[i].appendChild(theP);
+//     }
+//     if (i === 1){
+//         array[i].classList.add("unborderbox");
+//         //const node = document.createTextNode(" eef");
+//         theP.appendChild(node);
+//         array[i].appendChild(theP);
+//     }
+//     if (i === 2){
+//         array[i].classList.add("boxesWhite");
+//         //const node = document.createTextNode(" fdn");
+//         theP.appendChild(node);
+//         array[i].appendChild(theP);
+//     }
+//     if (i === 3){
+//         array[i].classList.add("boxesMaroon");
+//         //const node = document.createTextNode("g ");
+//         theP.appendChild(node);
+//         array[i].appendChild(theP);
+//     }
+//     if (i === 4){
+//         array[i].classList.add("boxesWhite");
+//         //const node = document.createTextNode("fdnsn ");
+//         theP.appendChild(node);
+//         array[i].appendChild(theP);
+//     }
+//     if (i === 5){
+//         array[i].classList.add("boxesMaroon");
+//         //const node = document.createTextNode(" bszsb");
+//         theP.appendChild(node);
+//         array[i].appendChild(theP);
+//     }
+// }
+// let counter = 0;
+// function dynamicCounter(){
+//     counter = counter + 1;
+// }
+// const counterElem = document.getElementById("counter");
+// //counterElem.createTextNode(counter);
+// let button = document.createElement("input");
+// button.type = "button";
+// button.addEventListener("click", dynamicCounter);
+// counterElem.appendChild(button);
